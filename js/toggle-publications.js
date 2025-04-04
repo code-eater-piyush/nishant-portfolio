bd4eeb9b158fddd1dd35b05c7f516e577fe012c3
Safal-Gupta <safalgupta040310@gmail.com>
2025-04-05 01:20:51 +0530

    designed further

--- js/toggle-publications.js
+++
@@ -1,8 +0,0 @@
-function toggleYear(year) {
-    var content = document.getElementById('year-' + year);
-    if (content.style.display === "none") {
-        content.style.display = "block";
-    } else {
-        content.style.display = "none";
-    }
-}
